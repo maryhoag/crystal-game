d3565e33e81096d39d06901eeaa08094192dcbce
Mary Hoag <marymcevoyhoag@gmail.com>
2016-04-22 23:09:21 -0400

    added comments

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//declare objects crystal, image, random number
+
+//choose random number and display
+
+//display wins and losses
+
+	//on click for each crystal image adds points to total
+
+	//exceed random number ==> lose
+
+//restart
+
+//reset score to 0;
+
+
