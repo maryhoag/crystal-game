73090ef99cae174a614682b17db74512fc9acf18
Mary Hoag <marymcevoyhoag@gmail.com>
2016-04-25 20:41:30 -0400

    added html and functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,20 @@
 //declare objects crystal, image, random number
+var shinyThings = [];
+
 
 //choose random number and display
+var getMagicNumber = function() {
+	Math.random() * (120 - 19) + 19;
+}
 
+//function getRandomArbitrary(min, max) {
+    //return Math.random() * (max - min) + min;
+//}
+var getCrystalNumber = function() {
+	//Math.floor ??
+	Math.random() * (12 -1) + 1;
+}
+;
 //display wins and losses
 
 	//on click for each crystal image adds points to total
